3b4ae53e44922b0e2e4a000e3087af52e0c52797
daisuke.iwasaki <daiemon.odakyu4000@gmail.com>
2024-05-02 10:04:55 +0900

    いいね実装とりあえず完成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -250,7 +250,7 @@ public class ReportAction extends ActionBase {
             //いいねボタンの更新
             rv.setGoodFlag(1);
 
-            // いいねフラグ1→0の場合
+        // いいねフラグ1→0の場合
         } else {
             //いいねボタンの更新
             rv.setGoodFlag(0);
@@ -258,6 +258,24 @@ public class ReportAction extends ActionBase {
 
         //日報データを更新する
         List<String> errors = service.update(rv);
+
+        if (errors.size() > 0) {
+            //更新中にエラーが発生した場合
+
+            putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+            putRequestScope(AttributeConst.REPORT, rv); //入力された日報情報
+            putRequestScope(AttributeConst.ERR, errors); //エラーのリスト
+
+            //編集画面を再表示
+            forward(ForwardConst.FW_REP_EDIT);
+        } else {
+            //更新中にエラーがなかった場合
+
+            //セッションに更新完了のフラッシュメッセージを設定
+            putSessionScope(AttributeConst.FLUSH, MessageConst.I_UPDATED.getMessage());
+
+        }
+
 
         //一覧画面にリダイレクト
         redirect(ForwardConst.ACT_REP, ForwardConst.CMD_INDEX);
--- src/main/java/constants/MessageConst.java
+++ src/main/java/constants/MessageConst.java
@@ -15,8 +15,6 @@ public enum MessageConst {
     I_REGISTERED("登録が完了しました。"),
     I_UPDATED("更新が完了しました。"),
     I_DELETED("削除が完了しました。"),
-    I_GOOD("いいねが押されました"),
-    I_GOOD_DELETED("いいねが削除されました"),
 
     //バリデーション
     E_NONAME("氏名を入力してください。"),
